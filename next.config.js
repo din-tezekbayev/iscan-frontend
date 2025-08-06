1a416469f4ddc89eb46600ad848caed20cfe4e8b
dinmukhamed <din.tezekbayev@foam.agency>
2025-08-06 10:30:20 +0500

    fix api

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'iscan-backend-production.up.railway.app',
   },
   // Railway deployment optimization
   output: 'standalone',
