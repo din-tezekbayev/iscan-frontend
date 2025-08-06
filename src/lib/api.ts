2e77eaabb7db4210faa181250c5090ef4769d39d
dinmukhamed <din.tezekbayev@foam.agency>
2025-08-06 10:25:13 +0500

    debug api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { FileType, FileRecord, Batch, TaskStatus } from '@/types';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
-
+console.log('process.env.NEXT_PUBLIC_API_URL', process.env.NEXT_PUBLIC_API_URL);
 const api = axios.create({
   baseURL: API_BASE_URL,
 });
